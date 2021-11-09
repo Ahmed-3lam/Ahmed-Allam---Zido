67a259cdbf57e9e9eb5f398641f209de1fc8e66c
Ahmed Allam <3lam.ahmedd@gmail.com>
2021-11-09 19:04:11 +0200

    update

--- lib/UI/main/cubit/main_cubit.dart
+++ lib/UI/main/cubit/main_cubit.dart
@@ -1,10 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:meta/meta.dart';
-import 'package:zido/Helpers/CommonUtils/image_utils.dart';
-import 'package:zido/Helpers/res/m_colors.dart';
 
 part 'main_state.dart';
 
